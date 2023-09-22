3a20a8a2247bbb3a6e50aab8ded56537b6beacba
solusmax <solusmaxim@gmail.com>
2023-09-22 14:32:06 +0300

    Make backuping "project.json" more convenient

--- bash/build.sh
+++ bash/build.sh
@@ -2,14 +2,20 @@ export WE_MY_PROJECTS="/c/Program Files (x86)/Steam/steamapps/common/wallpaper_e
 export GALLERY_PROJECT="gallery"
 export PROJECT_FILE="project.json"
 
-while getopts se option; do
+while getopts sec option; do
   case $option in
     s)
+    # Start
       sh bash/copyWEProjectJsonToPublic.sh
       sh bash/clearWEProject.sh
       ;;
     e)
+    # End
       sh bash/copyDistToWEProject.sh
+      ;;
+    c)
+    # Copy project.json
+      sh bash/copyWEProjectJsonToPublic.sh
       ;;
     \?)
       echo "Error: Invalid option"
